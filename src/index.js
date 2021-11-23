c3d65071f955f47bffeaeaf6fc4cb65b7ae0c6ea
Guilherme Araujo <guilhermepires.araujo@gmail.com>
2021-11-22 23:14:02 -0400

    primeiro commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/global.scss';
+import Header from './components/Header';
+
+
+//jsx
+
+ReactDOM.render(
+  <div className="main__wrapper">
+    <div className="main__container">
+      <Header/>
+
+    </div>
+
+  </div>
+  ,
+  document.getElementById('root')
+);
